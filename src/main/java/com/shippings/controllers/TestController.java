66de8fbced41bba968e92dc800eecc8f665aee01
Dasha <Dasha@DASHA-PC>
2020-04-08 17:51:52 +0300

    Водительская часть Есть Мои поездки: там список маршрутов текущего водителя со статусом true (в последствии сюда будет прикручена возможность редактирования) По нажатию кнопки + вызывается окно для добавления маршрута (нужно еще подправить, имеет какие-то проблемы с автоинкрементом) Есть Реквесты: список маршрутов водителя со статусом False (нужно докрутить кнопки)

--- src/main/java/com/shippings/controllers/TestController.java
+++ src/main/java/com/shippings/controllers/TestController.java
@@ -1,10 +1,25 @@
 package com.shippings.controllers;
 
+import com.shippings.model.User;
+import com.shippings.security.services.UserDetailsImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.shippings.model.Shipping;
+import com.shippings.repositories.ShippRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -22,9 +37,56 @@ public class TestController {
         return "Client Content.";
     }
 
+    @Autowired
+    private ShippRepository shippRepository;
+
+    private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
+
+    long getCurrentUserId() {
+        UserDetailsImpl user = (UserDetailsImpl)SecurityContextHolder.getContext().getAuthentication().getPrincipal();;
+        return user.getId();
+    }
+
     @GetMapping("/driver")
     @PreAuthorize("hasAuthority('driver')")
-    public String driverAccess() {
-        return "Driver Content.";
+    public List<Map<String, String>> driverAccess(){
+        LOG.info("driver request is received");
+        return getRouteList(Boolean.TRUE);
     }
+
+    @GetMapping("/driverRequest")
+    @PreAuthorize("hasAuthority('driver')")
+    public List<Map<String, String>> driverRequestAccess() {
+        LOG.info("driver request request is received.");
+       return getRouteList(Boolean.FALSE);
+    }
+
+    List<Map<String, String>> getRouteList(Boolean bool){
+        List<Map<String, String>> shippList = new ArrayList();
+        List<Shipping> shipps = shippRepository.findAllByDriverIdAndStatus(this.getCurrentUserId(), bool);
+        LOG.info(String.format("shipp.size is %d", shipps.size()));
+        for (Shipping s : shipps)
+            shippList.add(new HashMap<String, String>(){{
+                put("date_start", s.getDateStart().toString());
+                put("date_finish", s.getDateFinish().toString());
+                put("start", s.getStart());
+                put("finish", s.getFinish());
+                put("weight", Integer.toString(s.getWeight()));
+                put("height", Integer.toString(s.getHeight()));
+                put("length", Integer.toString(s.getLength()));
+                put("width", Integer.toString(s.getWidth()));
+                put("plus_time", Integer.toString(s.getPlusTime()));
+                put("comment", s.getComment());
+            }});
+
+        return shippList;
+    }
+
+    @GetMapping("/addRoute")
+    @PreAuthorize("hasAuthority('driver')")
+    public String addRouteAccess(){
+        LOG.info("addRoute");
+        return ";3";
+    }
+
 }
